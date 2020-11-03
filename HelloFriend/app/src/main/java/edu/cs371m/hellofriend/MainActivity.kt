d5e47190740965059bc7bd3cd17bbe1cbe63cdb7
Ye Yu <yeyuhere@gmail.com>
2020-11-02 21:47:12 -0500

    initial commit

---
+++ HelloFriend/app/src/main/java/edu/cs371m/hellofriend/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.cs371m.hellofriend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
